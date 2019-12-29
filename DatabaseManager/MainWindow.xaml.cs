d2f3bc1313e3461ed0f217680d57e88bfb0784fd
nikseljkee <nikseljkee1@gmail.com>
2019-12-29 10:58:38 +0300

    Init commit

---
+++ DatabaseManager/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace DatabaseManager
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены, что хотите выйти?", "Выход", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                this.Close();
+        }
+
+        private void SettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settingsWindow = new SettingsWindow();
+
+            if (settingsWindow.ShowDialog() == true)
+            {
+
+            }
+        }
+    }
+}
---
+++ DatabaseManager/SettingsWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace DatabaseManager
+{
+    /// <summary>
+    /// Логика взаимодействия для SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+
+            ConnectionStringBox.Text = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        private void CloseSettings_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void AcceptSettings_Click(object sender, RoutedEventArgs e)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var connectionStringsSection = (ConnectionStringsSection)config.GetSection("connectionStrings");
+            connectionStringsSection.ConnectionStrings["DefaultConnection"].ConnectionString = ConnectionStringBox.Text;
+            config.Save();
+            ConfigurationManager.RefreshSection("connectionStrings");
+
+            this.DialogResult = true;
+        }
+    }
+}
